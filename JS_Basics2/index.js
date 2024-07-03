d3f2afd8f3f803dd0eb4d1767e06df3b07907d17
Animesh Kumar <83393501+kmranimesh@users.noreply.github.com>
2024-07-03 16:29:51 +0530

    Factory Function, Constructor function, created objects with various ways

---
+++ JS_Basics2/index.js
@@ -0,0 +1,94 @@
+console.log('Hey Worldies');
+
+// let rectangle = {
+//     length: 1,
+//     breadth: 1,
+
+//     draw : function() {
+//         console.log('Drawing it');
+//     }
+// };
+
+// rectangle.draw();
+
+
+//factory function
+
+// function createRectangle(len, bre) {
+
+//     const rectangle = {
+//         length: len,
+//         breadth: bre,
+
+//         draw() {
+//             console.log('I can draw rectangle');
+//         }
+//     };
+//     return rectangle;
+// };
+
+// let rectangle1 = createRectangle(5, 5);
+// let rectangle2 = createRectangle(3, 6);
+
+
+// Constructor Function -> Pascal Notation is used and every word has capital letter as first
+
+// function Rectangle(len, bre) {
+//     this.length = len,
+//     this.breadth = bre,
+//     this.draw = function() {
+//         console.log('I can draw Rectangle');
+//     }
+// }
+
+// let object = new Rectangle(3, 4);
+
+// console.log(object);
+
+// object.color = 'yellow';
+// console.log(object);
+
+// delete object.color;
+// console.log(object);
+
+
+// let Rectangle = new Function (
+//     'length', 'breadth',
+//     `this.length = length,
+//     this.breadth = breadth,
+//     this.draw = function() {
+//     console.log('I can draw rectangle);
+//     }`);
+
+// //object creation using Rectangle1
+// let rect = new Rectangle(2,3);
+
+// rect.length;
+
+// console.log(rect);
+
+
+// rectangle.length;
+// rectangle.breadth;
+
+// rectangle.draw();
+
+// let rectangle1 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
+
+// let rectangle2 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
+
---
+++ JS_Basics2/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+
+// let rectangle2 = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw: function() {
+//         console.log('drawing rectangle');
+//     }
+// };
