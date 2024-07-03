28e9bae59ac094b3b969c3dbd721fcaf4cd46587
Animesh Kumar <83393501+kmranimesh@users.noreply.github.com>
2024-07-03 12:39:27 +0530

    Js Basics - 1 - loops/ variables

--- JS_Basics1/index.js
+++ JS_Basics1/index.js
@@ -1,7 +1,85 @@
 // linr below is used to print on the console window
 console.log('Pyaari Duniya');
 
-let a = 5;
-let name = 'Animesh';
+// let a = 5;
+// let name = 'Animesh';
 
-let 
+// let a = true;
+// console.log(a);
+
+// const num = 12;
+// num = 13;
+
+// let lastName = 3;
+// console.log(lastName);
+
+// lastName = true;
+// console.log(lastName);
+
+// console.log(5!==3);
+
+// let age = 15;
+// let status = (age >= 18) ? 'Can Vote' : 'Cannot Vote';
+
+// console.log(status);
+
+
+// If - else
+
+
+// let marks = 99;
+
+// if(marks >= 90) {
+//     console.log('Grade A');
+// }
+// else if(marks >= 80) {
+//     console.log("Grade B");
+// }
+// else if(marks >= 70) {
+//     console.log('Grade C');
+// }
+// else{
+//     console.log('Average');
+// }
+
+
+// Switch
+
+
+// let num = 2;
+
+// switch (num) {
+//     case 1: console.log('A');
+//         break;
+//     case 2: console.log('B');
+//         break;
+//     case 3: console.log('C');
+//         break;
+//     default: console.log('D');
+// }
+
+
+//for-loop
+
+// for (let i = 0; i <= 5; i++) {
+//     console.log('Anni' + i);
+// }
+
+
+//while - loop
+
+// let i = 1;
+// while(i <= 5){
+//     console.log(i);
+//     i++;
+// }
+
+
+// do-while loop
+
+// let y = 1;
+
+// do {
+//     console.log(y);
+//     y++;
+// } while (y <= 5)
